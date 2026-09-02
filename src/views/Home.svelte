3dbc20a82f724a7a11f06724f95535160244c3fc
Liv <abithefemboy@proton.me>
2026-09-02 17:56:10 +0200

    We got the layout!

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,5 +1,10 @@
 <div class="home_main">
-    
+    <div class="info_section">
+
+    </div>
+    <div class="actual_content">
+
+    </div>
 </div>
 <style>
     .home_main {
@@ -8,6 +13,18 @@
         background-color: #11111b;
         padding: 50px;
         box-sizing: border-box;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 40px;
+    }
+    .info_section {
+        width: 330px;
+        border: 1px solid #3f4153;
+    }
+    .actual_content {
+        width: 1000px;
+        background-color: white;
         
     }
 </style>
