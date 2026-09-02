10102ed46f66913b229c0a56058e317b14f312d2
Liv <abithefemboy@proton.me>
2026-09-02 17:51:40 +0200

    first styling

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,1 +1,13 @@
-<span>hi</span>
+<div class="home_main">
+    
+</div>
+<style>
+    .home_main {
+        width: 100%;
+        height: 100vh;
+        background-color: #11111b;
+        padding: 50px;
+        box-sizing: border-box;
+        
+    }
+</style>
